c841c8f0e435a766e9cd6e43e6b2d45a4a9ba6d4
KeYouheng <23302010044@m.fudan.edu.cn>
2025-03-16 16:23:01 +0800

    fix: fixed the issue that the page cannot jump

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ router.beforeEach((to, from, next) => {
 	if (tokenStore.isLoggedIn()) {
 		next()
 	} else {
-		if (to.path === '/login') {
+		if (to.path === '/login' || to.path === '/reset') {
 			next()
 		} else {
 			next('/login')
